26d536ae8295a7deacd57e3083ce2882bfe58030
Kyamal <hasisti@mail.ru>
2024-09-20 17:07:07 +0300

    test github actions

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from unittest.mock import patch
 from main import clean_text, correct_text_with_yandex_speller, transcribe_file
 
@@ -10,7 +11,7 @@ def test_clean_text():
 
 
 # Тест для проверки орфографии
-@patch('main.requests.get')
+@patch('requests.get')  # Исправлено на прямое обращение к requests
 def test_correct_text_with_yandex_speller(mock_get):
     # Имитация ответа от Яндекс.Спеллера
     mock_get.return_value.status_code = 200
@@ -23,7 +24,7 @@ def test_correct_text_with_yandex_speller(mock_get):
 
 
 # Тест для транскрипции (используем mock для whisper)
-@patch('main.whisper.load_model')
+@patch('whisper.load_model')  # Исправлено на прямое обращение к whisper
 def test_transcribe_file(mock_load_model):
     mock_model = mock_load_model.return_value
     mock_model.transcribe.return_value = {'text': 'Это транскрибированный текст'}
